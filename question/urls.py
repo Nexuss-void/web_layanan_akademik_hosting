bc3ca6d3f8846fd59a9286845260cfa369677640
Nexuss-void <channicholas8@gmail.com>
2026-07-17 09:32:41 +0700

    ~update

--- question/urls.py
+++ question/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:period_id>/<int:order_number>/', views.ui_kuesioner, name='ui_kuesioner'),
-    path('kuesioner/<int:period_id>/', views.start_kuesioner, name='start_kuesioner'),
+    path('<int:period>/<int:order_number>/', views.ui_kuesioner, name='ui_kuesioner'),
+    path('kuesioner/<int:period>/', views.start_kuesioner, name='start_kuesioner'),
 ]
