621b28dc280c366cd8a002a03d0d9f2c3c61cb15
Nexuss-void <channicholas8@gmail.com>
2026-08-07 03:08:33 +0700

    Update sistem V4.1

--- layanan_akademik_web/settings.py
+++ layanan_akademik_web/settings.py
@@ -17,6 +17,7 @@ import cloudinary
 # import cloudinary.api
 # import cloudinary.uploader
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
